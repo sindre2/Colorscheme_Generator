6122d34b0d0dc0f99e5a2891e37a171885f246f4
sindre2 <106583791+sindre2@users.noreply.github.com>
2022-08-03 11:28:24 +0200

    Updates for index
    August 3. Update

--- index.js
+++ index.js
@@ -1,40 +1,116 @@
-document.getElementById("getColor-Btn").addEventListener("click", schemeSelector);
+/* --- Constants and variables section --- */
+
+const hexOne = document.getElementById("hex-1");
+const hexTwo = document.getElementById("hex-2");
+const hexThree = document.getElementById("hex-3");
+const hexFour = document.getElementById("hex-4");
+const hexFive = document.getElementById("hex-5");
+
+const banan = document.querySelector("main");
+const eple = document.querySelector("footer");
+
+let colors = false;
+
+/* --- END --- */
+
+document.getElementById("getColor-Btn").addEventListener("click", decider);
+
+
+/* --- Function for clearing out existing color schemes and adding new schemes --- */
+
+function decider()
+{
+    if (colors)
+    {
+    banan.innerHTML = "";
+    eple.innerHTML = "";
+    schemeSelector();
+    } else
+    {
+        schemeSelector();
+    }
+}
+
+/* --- END --- */
+
+/* --- Selecting the newly generated schemes --- */
 
 function schemeSelector ()
 {
-    const tree = document.getElementById("gaynator").value.replace("#", "");
+    const tree = document.getElementById("getThis").value.replace("#", "");
     const flower = document.getElementById("generator").value;
 
-    console.log(tree);
-    console.log(flower);
 
     const end = `hex=${tree}&format=JSON&mode=${flower}&count=5`;
     
     getColorScheme(end);
 };
 
+/* --- END --- */
+
+/* --- Fetch function for new schemes --- */
 
 function getColorScheme (end)
 {
-    console.log("https://www.thecolorapi.com/scheme?" + end);
 
     fetch("https://www.thecolorapi.com/scheme?" + end)
         .then (response => response.json())
         .then (data => 
         {
             
-            let temp = data
-            console.log(temp.colors[2].hex.value);
-            for (x = 0; x < temp.colors.length; x++)
+            let temp = data;
+
+            for (let x = 0; x < temp.colors.length; x++)
             {
-                document.getElementById("addHere").innerhtml += 
-                `<div style="background-color: ${temp.colors[x].hex.value}">
-                <p> LOL THIS WAS ADDED </p>
-                </div>`
+                banan.innerHTML += 
+                `<div class="col" style="background-color: ${temp.colors[x].hex.value}">
+                </div>`;
+
+                document.getElementById("hex-" + x).textContent +=  `${temp.colors[x].hex.value}`;
+                 
             };
+
+            return colors = true, copy = true;
         }
     )
 };
 
+/* --- END --- */
 
+
+
+// short.addEventListener("click", function()
+// {
+
+// })
+
+hexOne.addEventListener("click", function()
+    {
+        navigator.clipboard.writeText(hexOne.textContent);
+        alert("Copied the text: " + hexOne.textContent);
+    });
+
+hexTwo.addEventListener("click", function()
+    {
+        navigator.clipboard.writeText(hexTwo.textContent);
+        alert("Copied the text: " + hexTwo.textContent);
+    });
+
+hexThree.addEventListener("click", function()
+    {
+        navigator.clipboard.writeText(hexThree.textContent);
+        alert("Copied the text: " + hexThree.textContent);
+    });
+
+hexFour.addEventListener("click", function()
+    {
+        navigator.clipboard.writeText(hexFour.textContent);
+        alert("Copied the text: " + hexFour.textContent);
+    });
+
+hexFive.addEventListener("click", function()
+    {
+        navigator.clipboard.writeText(hexFive.textContent);
+        alert("Copied the text: " + hexFive.textContent);
+    });
 
